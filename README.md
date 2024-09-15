dbe51afc0e61fd6691dc9edefc1d2c7085116731
MariaPaez <mpaezgrecco@gmail.com>
2024-09-14 23:19:55 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# PI1_Recomendacion_Peliculas
+# **PROYECTO INDIVIDUAL Nº1**
+
+## Machine Learning Operations (MLOps)
+### **Descripción**
+#### El Proyecto Individual 1 es una API desarrollada con FastAPI que ofrece datos estadísticos, consultas, y recomendaciones personalizadas de películas. Permite explorar información detallada de filmaciones, actores y directores, con un sistema de recomendación basado en puntajes, géneros y títulos.
+
+### **Tabla de contenido**
+#### 
+1. Introduccion 
+2. Instalación y Requisitos
+3. Estructura del Proyecto
+4. Uso y Ejecución
+5. Datos y Fuentes
+6. Metodología
+7. Resultados y Conclusiones
+8. Contribución y Colaboración
+9. Licencia
+
+### **Introduccion**
+#### Este proyecto presenta una aplicación basada en una API que ofrece información detallada sobre películas y funcionalidades, como consultas y recomendaciones. Desarrollada utilizando el framework FastAPI, la aplicación permite a los usuarios acceder a datos estadísticos, como la cantidad de filmaciones en un mes, puntajes y número de votaciones, así como explorar información relacionada con actores y directores. Incluye un sistema de recomendación basado en la similitud de puntuaciones, géneros y títulos. Se llevó a cabo un análisis exploratorio de los datos y la aplicación fue desplegada en Render. Al final, se incluye un video que demuestra su funcionamiento y las consultas implementadas.
+
+### Instalación y Requisitos
+#### Requisitos:
+- Python 3.11 
+- pandas
+- numpy
+- matplotlib
+- scikit-learn
+- FastApi
+- uvicorn 
+
+#### Pasos de instalación:
+
+- Clonar el repositorio: git clone https://github.com/mpaezgrecco/PI1_Recomendacion_Peliculas.git
+- Crear un entorno virtual: python -m venv venv
+- Activar el entorno virtual:
+Windows: venv\Scripts\activate
+macOS/Linux: source venv/bin/activate
+- Instalar las dependencias: pip install -r requirements.txt
+
+### Estructura del Proyecto
+####
+- data/: Contiene los archivos de datos utilizados en el proyecto 
+- README.md: Archivo de documentación del proyecto.
+- PI1_preparacion : Archivo donde se encuentra el proceso de ETL 
+- PI1_EDA : Archivo donde se encuentra el analisis exploratorio de los datos
+- main : Archivo donde se encuentra el codigo para crear la API de forma local utilizando fastAPI.
+- sistema_de_recomendacion_ML: En este archivo se encuentra el 
--- main.py
+++ main.py
@@ -27,15 +27,18 @@ data['name_genres'] = data['name_genres'].apply(lambda x: ' '.join(eval(x)))
 1. Stopwords personalizadas: Se eliminan palabras comunes que no aportan valor semántico relevante, 
 como "the","of", "and" lo que ayuda a mejorar la precisión del modelo de recomendación.'''
 
-stopwords_custom = ["the","of", "and"]  
+custom_stop_words = [
+    'the', 'and', 'to', 'of', 'in', 'a', 'for', 'on', 'with', 'as', 'is', 'at', 'this'
+]  
 
 ''' 2. TF-IDF (Term Frequency-Inverse Document Frequency) es una técnica utilizada para evaluar la relevancia de una 
 palabra en un documento dentro de un corpus. En este caso, se aplica a los títulos de las películas para 
 representar cada título como un vector en un espacio multidimensional, donde cada dimensión corresponde a 
 una palabra.'''
 
-tfidf = TfidfVectorizer(stop_words=stopwords_custom)
-tfidf_matrix = tfidf.fit_transform(data['title'])
+# Vectorización de texto con eliminación de stopwords
+vectorizer = TfidfVectorizer(stop_words=custom_stop_words)
+tfidf_matrix = vectorizer.fit_transform(data['title'])
 
 # Calcular la similitud del coseno entre los títulos de las películas
 cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix)
@@ -43,9 +46,13 @@ cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix)
 ''' 1. def cantidad_filmaciones_mes( Mes ): 
 Se ingresa un mes en idioma Español. Debe devolver la cantidad de películas que fueron estrenadas en el
 mes consultado en la totalidad del dataset.'''
-
-# Mapeo de los meses en español a números
-meses = {
+@app.get("/cantidad_film_mes/{mes}")
+def cantidad_film_mes(mes: str):
+    # Convertir el mes a minúsculas para evitar problemas con las mayúsculas
+    mes = mes.lower()
+    
+    # Mapeo de los meses en español a números
+    meses = {
     "enero": 1,
     "febrero": 2,
     "marzo": 3,
@@ -59,11 +66,6 @@ meses = {
     "noviembre": 11,
     "diciembre": 12
 }
-
-@app.get("/cantidad_film_mes/{mes}")
-def cantidad_film_mes(mes: str):
-    # Convertir el mes a minúsculas para evitar problemas con las mayúsculas
-    mes = mes.lower()
 
     # Verificar si el mes ingresado es válido
     if mes not in meses:
@@ -83,58 +85,57 @@ def cantidad_film_mes(mes: str):
 Se ingresa un día en idioma Español. Debe devolver la cantidad de películas que fueron estrenadas
 en día consultado en la totalidad del dataset.'''
 
-# Mapeao los nombres de los dias en español a los números de los dias
-
-dias_semana = {"lunes": 0,"martes": 1,"miércoles": 2,"jueves": 3,
-                   "viernes": 4,"sábado": 5,"domingo": 6}
-
-# función
+#Función
 @app.get("/cantidad_filmaciones_dia/{dia}")
 def cantidad_filmaciones_dia(dia: str): # Devuelve la cantidad de filmaciones 
     contador = 0
-    
-     # Convertir el dia a minúsculas para evitar problemas con las mayusculas
+
+    # Mapear los nombres de los dias en español a los números de los dias
+    dias_semana = {"lunes": 0,"martes": 1,"miercoles": 2,"jueves": 3,
+                   "viernes": 4,"sabado": 5,"domingo": 6}
+
+    # Convertir el dia a minúsculas
     dia = dia.lower()
-    
-    # Verificar si el mes ingresado es válido
+
+    # Verificar si el dia ingresado es válido
     if dia not in dias_semana:
-        return {"error": "dia no válido"}
-        
-    #a continuacion el codigo cuenta el número de películas estrenadas en un día específico de la semana basado
-    # en el campo release_date de un dataset.'''
+        return f"Dia inválido: {dia}"
+    
+    # Aquí deberías tener la lógica para obtener los datos del dataset y contar las películas estrenadas en el día consultado
+    # Asegúrate de tener la estructura adecuada del dataset y ajusta el código según tus necesidades
+    for fecha_estreno in data["release_date"]:
+        fecha_estreno_obj = datetime.datetime.strptime(fecha_estreno, "%Y-%m-%d") # strptime se utiliza para convertir una cadena de texto en un objeto de fecha y hora (datetime).
 
-    for fecha_estreno in data["release_date"]: #recorre cada fecha en la columna release_date del DataFrame data.
-        fecha_estreno_obj = datetime.datetime.strptime(fecha_estreno, "%Y-%m-%d") #convierte una cadena de texto con formato "YYYY-MM-DD" en un objeto datetime. Esto permite manipular y comparar fechas fácilmente.
+        if fecha_estreno_obj.weekday() == dias_semana[dia]: # La función weekday() se utiliza para obtener el día de la semana correspondiente a un objeto datetime. Retorna un número entero que representa el día de la semana, donde el lunes es el día 0 y el domingo es el día 6.
+            contador += 1
 
-        if fecha_estreno_obj.weekday() == dias_semana[dia]: #Obtener el día de la semana y contar si coincide con el día especificado
-            contador += 1 # Si el día de la semana coincide con el día consultado, incrementa el contador.
+    # return f"{contador} películas fueron estrenadas en los días {dia.capitalize()}" # capitalize() convierte el primer carácter de una cadena en mayúscula y el resto de los caracteres en minúscula.
+    return {'dia':dia.capitalize(), 'cantidad':contador}
 
-    #Retornar el resultado
-    #dia.capitalize() convierte el primer carácter del día en mayúscula y el resto en minúscula.
-    # Devuelve un diccionario con el nombre del día y el número total de películas estrenadas en ese día de la semana.
-    return {'dia':dia.capitalize(), 'cantidad':contador} 
 
 '''3. def score_titulo( titulo_de_la_filmación ): 
 Se ingresa el título de una filmación esperando como respuesta el título, el año de estreno y el score.'''
 
-#funcion 
+#funcion
 @app.get("/score_titulo/")
 def score_titulo(titulo_de_la_filmación: str):
     
- # Buscar la fila en el DataFrame que coincide con el título proporcionado
-    pelicula = data[data['title'] == titulo_de_la_filmación]
+    # Normalizar el título de entrada: convertir a minúsculas y eliminar espacios extra
+    titulo_de_la_filmación = titulo_de_la_filmación.strip().lower()
+    
+    # Buscar la fila en el DataFrame que coincide con el título proporcionado
+    pelicula = data[data['title'].str.lower().str.strip() == titulo_de_la_filmación]
 
     # Verificar si se encontró alguna película con el título dado
-    if not pelicula.empty: # El método empty devuelve True si el DataFrame está vacío.
+    if pelicula.empty:  # El método empty devuelve True si el DataFrame está vacío
+        return {"error": "Película no encontrada"}
         
-        # Obtener el año de estreno y el score de la película encontrada
-        titulo = pelicula['title'].values[0] 
-        año_estreno = pelicula['release_year'].values[0]
-        score = pelicula['vote_average'].values[0]
-        # return f"La pelicula {titulo} fue estrenada en el año {año_estreno} con un score de {score}."
-        return {'titulo':titulo, 'anio':año_estreno, 'popularidad':score}
-    else:
-        raise HTTPException(status_code=404, detail="Título no encontrado")
+    # Obtener el año de estreno y el score de la película encontrada
+    titulo = pelicula['title'].iloc[0]  # iloc[0] sirve para acceder al primer registro
+    año_estreno = str(pelicula['release_year'].iloc[0])
+    score = str(pelicula['popularity'].iloc[0])
+    
+    return {'titulo': titulo, 'anio': año_estreno, 'popularidad': score}
 
 ''' 4. def votos_titulo( titulo_de_la_filmación ):
 Se ingresa el título de una filmación esperando como respuesta el título, la cantidad de votos y el valor 
@@ -143,29 +144,22 @@ debemos contar con un mensaje avisando que no cumple esta condición y que por e
 
 @app.get("/votos_titulo/")
 def votos_titulo(titulo_de_la_filmación: str):
-    # Buscar la fila en el DataFrame que coincide con el título proporcionado
+    #buscar la fila en el DataFrame que coincide con el título proporcionado
     pelicula = data[data['title'] == titulo_de_la_filmación]
-
- # Verificar si se encontró alguna película con el título dado
-    if not pelicula.empty:
-        cantidad_votos = pelicula['vote_count'].values[0]
-        promedio_votos = pelicula['vote_average'].values[0]
+    #Verificar si se encontró alguna película con el título dado
+    if pelicula.empty:
+        return "Pelicula no encontrada."    
+    # Verificar si la película tiene al menos 2000 valoraciones
+    votos = pelicula['vote_count'].iloc[0]
+    titulo =pelicula['title'].iloc[0] 
+    promedio_votos= pelicula['vote_average'].iloc[0]
+    año_estreno= str(pelicula['release_year'].iloc[0])
         
-         # Verificar si la película tiene al menos 2000 valoraciones
-        if cantidad_votos >= 2000:
-            return {
-                'titulo': titulo_de_la_filmación,
-                'cantidad_votos': cantidad_votos,
-                'promedio_votos': promedio_votos
-            }
-        else:
-            raise HTTPException(
-                status_code=400,
-                detail="La película no cumple con la condición de tener al menos 2000 valoraciones."
-            )
+    if votos < 2000:
+        return f"La película no cumple con la condición de tener al menos 2000 valoraciones, esta pelicula cuenta con {int(votos)} votos"
     else:
-        raise HTTPException(status_code=404, detail="Título no encontrado")
-        
+        return {'titulo':titulo, 'anio':año_estreno, 'voto_total':votos, 'voto_promedio':promedio_votos}
+    
 ''' def get_actor( nombre_actor ): 
 Se ingresa el nombre de un actor que se encuentre dentro de un dataset debiendo devolver el éxito del
 mismo medido a través del retorno. Además, la cantidad de películas que en las que ha participado y el
@@ -219,32 +213,26 @@ def get_director(nombre_del_director: str):
     return respuesta
 
 @app.get('/recomendacion/{titulo}')
-def recomendacion(titulo: str):
-    # Verificar si el título está en el DataFrame
+def recomendacion(titulo: str):    
+     # Verificar si el título está en el DataFrame
     if titulo not in data['title'].values:
-        return {"message": f"No se encontró ninguna película con el título '{titulo}'."}
-
-    # Crear una matriz TF-IDF para los títulos de las películas
-    tfidf = TfidfVectorizer(stop_words="english")
-    tfidf_matrix = tfidf.fit_transform(data['title'])
-
-    # Calcular la matriz de similitud de coseno
-    cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix)
+        return {'error': f"No se encontró el título '{titulo}'."}
 
     # Encontrar el índice de la película con el título dado
-    idx = data[data['title'] == titulo].index[0]
+    idx = data.index[data['title'] == titulo].tolist()[0]
 
-    # Calcular las puntuaciones de similitud de todas las películas
+    # Calcular las puntuaciones de similitud de todas las películas con la película dada
     sim_scores = cosine_similarities[idx]
 
-    # Obtener los índices de las 5 películas más similares, excluyendo la original
-    sim_indices = sim_scores.argsort()[-6:-1][::-1]
+    # Ordenar las películas por puntaje de similitud en orden descendente
+    sim_scores = sim_scores.argsort()[::-1]  # Índices ordenados en orden descendente
+
+    # Obtener los índices de las películas más similares 
+    sim_scores = sim_scores[sim_scores != idx][:6]  # Excluyendo la película buscada y tomando las 5 mejores puntajes
 
     # Devolver los títulos de las películas más similares
-    respuesta_recomendacion = data['title'].iloc[sim_indices].tolist()
-    
-    return {'lista_recomendada': respuesta_recomendacion}
-
+    respuesta_recomendacion = data['title'].iloc[sim_scores].tolist()
+    return {'lista recomendada': respuesta_recomendacion}
 
 # Ejecutar la aplicación con Uvicorn
 if __name__ == '__main__':
